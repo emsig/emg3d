88f73115b1c9b8682b4555ee43040a613cd7b21a
Dieter Werthmüller <prisae@users.noreply.github.com>
2019-07-01 11:09:58 +0200

    Use `scooby` for `Versions` (#16)

--- emg3d/utils.py
+++ emg3d/utils.py
@@ -24,41 +24,12 @@ Utility functions for the multigrid solver.
 
 
 import os
-import sys
-import time
-import numba
-import scipy
+import scooby
 import shelve
-import textwrap
-import platform
 import numpy as np
-import multiprocessing
 from timeit import default_timer
 from datetime import datetime, timedelta
 from scipy import optimize, interpolate, ndimage
-
-import emg3d  # emg3d for Versions
-
-# Optional modules
-try:
-    import IPython
-except ImportError:
-    IPython = False
-try:
-    import matplotlib
-except ImportError:
-    matplotlib = False
-try:
-    import mkl
-except ImportError:
-    mkl = False
-
-# Get mkl info, if available
-if mkl:
-    mklinfo = mkl.get_version_string()
-else:
-    mklinfo = False
-
 
 __all__ = ['Model', 'Field', 'get_domain', 'get_stretched_h', 'get_hx',
            'get_source_field', 'get_receiver', 'get_h_field', 'TensorMesh',
@@ -1435,26 +1406,20 @@ def data_read(fname, keys=None, path="data"):
 
 
 # OTHER
-class Versions:
+class Versions(scooby.Report):
     r"""Print date, time, and version information.
 
-    Print date, time, and package version information in any environment
-    (Jupyter notebook, IPython console, Python console, QT console), either as
-    html-table (notebook) or as plain text (anywhere).
+    Use scooby to print date, time, and package version information in any
+    environment (Jupyter notebook, IPython console, Python console, QT
+    console), either as html-table (notebook) or as plain text (anywhere).
 
-    Always shown are the OS, number of CPU(s), ``numpy``, ``scipy``,
-    ``emg3d``, ``numba``, ``sys.version``, and time/date.
+    Always shown are the OS, number of CPU(s), ``numpy``, ``scipy``, ``emg3d``,
+    ``numba``, ``sys.version``, and time/date.
 
     Additionally shown are, if they can be imported, ``IPython`` and
     ``matplotlib``. It also shows MKL information, if available.
 
-    All modules provided in ``add_pckg`` are also shown. They have to be
-    imported before ``versions`` is called.
-
-    This script was heavily inspired by:
-
-        - ipynbtools.py from qutip https://github.com/qutip
-        - watermark.py from https://github.com/rasbt/watermark
+    All modules provided in ``add_pckg`` are also shown.
 
 
     Parameters
@@ -1464,8 +1429,14 @@ class Versions:
         imported beforehand).
 
     ncol : int, optional
-        Number of package-columns in html table; only has effect if
-        ``mode='HTML'`` or ``mode='html'``. Defaults to 3.
+        Number of package-columns in html table (no effect in text-version);
+        Defaults to 3.
+
+    text_width : int, optional
+        The text width for non-HTML display modes
+
+    sort : bool, optional
+        Sort the packages when the report is shown
 
 
     Examples
@@ -1479,139 +1450,13 @@ class Versions:
 
     """
 
-    def __init__(self, add_pckg=None, ncol=4):
-        """Initiate and add packages and number of columns to self."""
-        self.add_pckg = self._get_packages(add_pckg)
-        self.ncol = int(ncol)
+    def __init__(self, add_pckg=None, ncol=3, text_width=80, sort=False):
+        """Initiate a scooby.Report instance."""
 
-    def __repr__(self):
-        r"""Plain-text version information."""
+        # Mandatory packages.
+        core = ['numpy', 'scipy', 'numba', 'emg3d']
 
-        # Width for text-version
-        n = 54
-        text = '\n' + n*'-' + '\n'
+        # Optional packages.
+        optional = ['IPython', 'matplotlib']
 
-        # Date and time info as title
-        text += time.strftime('  %a %b %d %H:%M:%S %Y %Z\n\n')
-
-        # OS and CPUs
-        text += '{:>15}'.format(platform.system())+' : OS\n'
-        text += '{:>15}'.format(multiprocessing.cpu_count())+' : CPU(s)\n'
-
-        # Loop over packages
-        for pckg in self.add_pckg:
-            text += '{:>15} : {}\n'.format(pckg.__version__, pckg.__name__)
-
-        # sys.version
-        text += '\n'
-        for txt in textwrap.wrap(sys.version, n-4):
-            text += '  '+txt+'\n'
-
-        # mkl version
-        if mklinfo:
-            text += '\n'
-            for txt in textwrap.wrap(mklinfo, n-4):
-                text += '  '+txt+'\n'
-
-        # Finish
-        text += n*'-'
-
-        return text
-
-    def _repr_html_(self):
-        """HTML-rendered version information."""
-
-        # Define html-styles
-        border = "border: 2px solid #fff;'"
-
-        def colspan(html, txt, ncol, nrow):
-            r"""Print txt in a row spanning whole table."""
-            html += "  <tr>\n"
-            html += "     <td style='text-align: center; "
-            if nrow == 0:
-                html += "font-weight: bold; font-size: 1.2em; "
-            elif nrow % 2 == 0:
-                html += "background-color: #ddd;"
-            html += border + " colspan='"
-            html += str(2*ncol)+"'>%s</td>\n" % txt
-            html += "  </tr>\n"
-            return html
-
-        def cols(html, version, name, ncol, i):
-            r"""Print package information in two cells."""
-
-            # Check if we have to start a new row
-            if i > 0 and i % ncol == 0:
-                html += "  </tr>\n"
-                html += "  <tr>\n"
-
-            html += "    <td style='text-align: right; background-color: #ccc;"
-            html += " " + border + ">%s</td>\n" % version
-
-            html += "    <td style='text-align: left; "
-            html += border + ">%s</td>\n" % name
-
-            return html, i+1
-
-        # Start html-table
-        html = "<table style='border: 3px solid #ddd;'>\n"
-
-        # Date and time info as title
-        html = colspan(html, time.strftime('%a %b %d %H:%M:%S %Y %Z'),
-                       self.ncol, 0)
-
-        # OS and CPUs
-        html += "  <tr>\n"
-        html, i = cols(html, platform.system(), 'OS', self.ncol, 0)
-        html, i = cols(html, multiprocessing.cpu_count(), 'CPU(s)',
-                       self.ncol, i)
-
-        # Loop over packages
-        for pckg in self.add_pckg:
-            html, i = cols(html, pckg.__version__, pckg.__name__, self.ncol, i)
-        # Fill up the row
-        while i % self.ncol != 0:
-            html += "    <td style= " + border + "></td>\n"
-            html += "    <td style= " + border + "></td>\n"
-            i += 1
-        # Finish row
-        html += "  </tr>\n"
-
-        # sys.version
-        html = colspan(html, sys.version, self.ncol, 1)
-
-        # mkl version
-        if mklinfo:
-            html = colspan(html, mklinfo, self.ncol, 2)
-
-        # Finish table
-        html += "</table>"
-
-        return html
-
-    @staticmethod
-    def _get_packages(add_pckg):
-        r"""Create list of packages."""
-
-        # Mandatory packages
-        pckgs = [np, scipy, numba, emg3d]
-
-        # Optional packages
-        for module in [IPython, matplotlib]:
-            if module:
-                pckgs += [module]
-
-        # Cast and add add_pckg
-        if add_pckg is not None:
-
-            # Cast add_pckg
-            if isinstance(add_pckg, tuple):
-                add_pckg = list(add_pckg)
-
-            if not isinstance(add_pckg, list):
-                add_pckg = [add_pckg, ]
-
-            # Add add_pckg
-            pckgs += add_pckg
-
-        return pckgs
+        super().__init__(add_pckg, core, optional, ncol, text_width, sort)
