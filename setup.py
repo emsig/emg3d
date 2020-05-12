650e610a8bc85fafe5b925f47f36356ab919e398
prisae <mail@werthmuller.org>
2020-05-12 15:51:36 +0200

    Simplify requirements; avoid duplication

--- setup.py
+++ setup.py
@@ -25,7 +25,6 @@ setup(
         'Programming Language :: Python :: 3.8',
     ],
     install_requires=[
-        'numpy',
         'scipy>=1.4.0',
         'numba>=0.45.0',
         'empymod>=2.0.0',
