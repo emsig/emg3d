ae7021d79d4bafca7fbf8316b1c809f274659da2
prisae <mail@werthmuller.org>
2020-05-20 21:15:32 +0200

    EOW

--- emg3d/survey.py
+++ emg3d/survey.py
@@ -24,6 +24,8 @@ A survey combines a set of sources and receivers.
 
 
 # import numpy as np
+# from typing import Tuple
+# from dataclasses import dataclass
 
 # from emg3d import maps, models
 
@@ -85,26 +87,108 @@ class Survey():
     # nrec, nsrc, nfreq
     # size: actual data, which can be less than nsrc*nrec*nfreq
 
+    # data
+
+    # verify sources and receivers, that all exist, and that there are no
+    # unused sources or receivers.
+
 
 class Dipole():
-    # location
-    pass
+
+    def __init__(self, names, coordinates=None, **kwargs):
+        # Make it possible for either names and coordinates, or names as dict
+        # with name as keys and coordinates as values
+
+        # expand coordinates
+
+        # Move to add
+        self._names = dict()
+        for name in names:
+            self._names[name] = dict()
+
+        # Move to add
+        if len(coordinates) == 5:
+            print("point dipole")
+            self.is_finite = False
+        elif len(coordinates) == 6:
+            print("finite length dipole")
+            self.is_finite = True
+        else:
+            print("* ERROR   :: Dipole coordinates are wrong defined.\n"
+                  "             They must describe either a point,\n"
+                  "             (x, y, z, azimuth, dip), or a finite dipole,\n"
+                  "             (x1, x2, y1, y2, z1, z2)\n."
+                  f"            Provided coordinates: {coordinates}.")
+            raise ValueError("coordinates")
+
+    def names(self):
+        return self._names
 
 
 class DipoleSource(Dipole):
-    # which receivers
+
+    def __init__(self, names, coordinates=None, **kwargs):
+        # location
+
+        # Connect receivers
+        # Only name is required
+
+        super().__init__(names, coordinates, **kwargs)
+
     # what frequencies
     # nfreq
     # nrec
-    pass
+
+    # Method verify, to check that all connected receivers exist
+
+    # __repr__ method to print info
+
+    # to_dict/from_dict
 
 
 class DipoleReceiver(Dipole):
-    # which sources
+
+    def __init__(self, names=None, coordinates=None, **kwargs):
+        # location
+
+        # `Dipole` permits len in [5, 6].
+        # However, receivers are only implemented as points so far.
+
+        if len(coordinates) == 6:
+            print("* ERROR   :: Receivers are only implemented for point "
+                  "dipoles\n"
+                  "             at the moment, but finite length coordinates\n"
+                  "             (x1, x2, y1, y2, z1, z2) were provided.")
+            # TODO don't fail with an error, convert to point dipoles!
+            raise ValueError("coordinates")
+
+        # Connect sources and frequencies
+        # Only name is required
+
+        super().__init__(names, coordinates, **kwargs)
+
     # what frequencies
     # measured data
     # nsrc
     # nfreq
-    # get e-field
-    # get h-field
-    pass
+    # get e-field => needs survey
+    # get h-field => needs survey
+
+    # Method verify, to check that all connected sources exist with the
+    # appropriate frequencies.
+
+    # __repr__ method to print info
+
+    # to_dict/from_dict
+
+
+# @dataclass
+# class NewReceiver:
+#     name: str
+#     coordinates: Tuple[float, float, float]
+#     azimuth: float
+#     dip: float
+
+# @dataclass
+# class NewSource:
+#     receivers: List[NewReceiver]
