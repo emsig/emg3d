e1a4dfa31fcc02425574f20776a1b6d49669d833
prisae <mail@werthmuller.org>
2020-06-02 06:15:16 +0200

    EOD

--- emg3d/survey.py
+++ emg3d/survey.py
@@ -35,72 +35,87 @@ class Survey:
     """Create a survey with sources and receivers.
 
     A survey contains sources and receivers, which, on their own, do not say
-    anything about data. The data added must then be provided with the info
-    at which receiver it was measured, from which source comes the signal, and
+    anything about data. The data added must then be provided with the info at
+    which receiver it was measured, from which source comes the signal, and
     what is the source frequency.
 
     This survey is optimised for a node-based, marine CSEM survey. It stores
     the date in an array of size (nsrc, nrec, nfreq), it is therefore most
     compact if each receiver has measurements for each source and each
-    frequency. NaN's are placed where there is no data. THe survey is therefore
+    frequency. NaN's are placed where there is no data. The survey is therefore
     not good for a survey-type like a streamer-based CSEM survey, as there
     would be a huge matrix required with mostly NaN's. If this is required
     either a new survey-class has to be created, or this one has to be
-    adjusted.
+    adjusted. Probably with a `DataSet` where each source is a new `DataArray`.
 
-    TODO: Think about a reprocity flag?
+    Currently, it has only one `DataArray`, the data, but we should add
+    'noise', and maybe 'active' and others.
 
-    Currently, it has only the 'data' as variable, but we should add 'noise',
-    and maybe 'active' and others.
+    There might be better ways to incorporate `xarray`. To be improved...
+
+    TODO: Think about a reciprocity flag?
 
 
     Parameters
     ----------
     ?
+    # - either dict, containing Dipoles
+    # - or 6-element tuples (name, x, y, z, azimuth, dip)
+    # - or 7-element tuples (name, x0, x1, y0, y1, z0, z1)
+
+    # TODO, change. data: ndarray or None
 
     """
 
-    def __init__(self, name, sources=None, receivers=None, frequencies=None,
-                 data=None):
+    def __init__(self, name, sources, receivers, frequencies, data=None):
         """Initiate a new Survey instance."""
 
         # Survey name.
         self.name = name
 
-        # TODO, change. sources, receivers, frequencies: {dicts, tuples, None}
-        # - either dict, containing Dipoles
-        # - or 6-element tuples (name, x, y, z, azimuth, dip)
-        # - or 7-element tuples (name, x0, x1, y0, y1, z0, z1)
-
-        # TODO, change. data: ndarray or None
-
         # Initiate sources.
-        if sources is None:
-            self.sources = {}
-        else:
-            self.sources = {
+        if isinstance(sources, tuple):
+            self._sources = {}
+            self._add_sources(sources[0], sources[1:])
+        elif isinstance(sources, dict):
+            self._sources = {
                     k: Dipole.from_dict(v) for k, v in sources.items()}
+        else:
+            raise ValueError
 
         # Initiate receivers.
-        if receivers is None:
-            self.receivers = {}
-        else:
-            self.receivers = {
+        if isinstance(receivers, tuple):
+            self._receivers = {}
+            self._add_receivers(receivers[0], receivers[1:])
+        elif isinstance(receivers, dict):
+            self._receivers = {
                     k: Dipole.from_dict(v) for k, v in receivers.items()}
+        else:
+            raise ValueError
 
         # Initiate frequencies.
-        if frequencies is None:
-            self.frequencies = {}
-        else:
-            self.frequencies = {float(k): {} for k, v in frequencies.items()}
+        self._frequencies = np.array(frequencies, dtype=float)
 
-        # Initiate data.
-        self._data = None
-        if data is not None:
-            for src in data.keys():
-                for rec in data[src].keys():
-                    for freq in data[src][rec].keys():
-                        self.add_data(src, rec, freq, data[src][rec][freq])
+        # TODO:  add back the dict-possibility.
+        #
+        # TODO check to_dict/from_dict; save
+
+        # Initialize xarray.
+        if data is None:
+            data = np.ones((len(self._sources), len(self._receivers),
+                            len(self.frequencies)), dtype=complex)*np.nan
+
+        self._ds = xr.Dataset(
+                {'data': xr.DataArray(
+                    data,
+                    dims=('sources', 'receivers', 'frequencies'))},
+                coords={
+                    'sources': list(self.sources.keys()),
+                    'receivers': list(self.receivers.keys()),
+                    'frequencies': list(self.frequencies)
+                    },
+                )
+
 
     def __repr__(self):
         return (f"{self.__class__.__name__}: {self.name}\n"
@@ -201,7 +216,7 @@ class Survey:
             print(f"* ERROR   :: Variable {e} missing in `inp`.")
             raise
 
-    def add_receivers(self, names, coordinates):
+    def _add_receivers(self, names, coordinates):
         """Add receiver(s) to the survey.
 
         It accepts point dipoles and finite length dipoles. However, obtaining
@@ -219,11 +234,6 @@ class Survey:
 
         """
 
-        if self._data is not None:
-            print("Receivers can, currently, only be added before the\n"
-                  "data was initialized.")
-            return NotImplemented
-
         # Recursion for multiple receivers.
         if isinstance(names, list):
 
@@ -233,21 +243,21 @@ class Survey:
 
             # Loop over names.
             for i, name in enumerate(names):
-                self.add_receivers(name, coords[:, i])
+                self._add_receivers(name, coords[:, i])
 
         else:
             # Warn about duplicate names.
-            if names in self.receivers:
-                old_coords = self.receivers[names].coordinates
+            if names in self._receivers:
+                old_coords = self._receivers[names].coordinates
                 new_coords = tuple(np.array(coordinates, dtype=float))
                 print(f"* WARNING :: Overwriting existing receiver <{names}>:"
                       f"\n             - Old: {old_coords}\n"
                       f"             - New: {new_coords}")
 
             # Create a receiver dipole.
-            self.receivers[names] = Dipole(names, coordinates)
+            self._receivers[names] = Dipole(names, coordinates)
 
-    def add_sources(self, names, coordinates):
+    def _add_sources(self, names, coordinates):
         """Add source(s) to the survey.
 
         # strength : float
@@ -270,11 +280,6 @@ class Survey:
 
         """
 
-        if self._data is not None:
-            print("Sources can, currently, only be added before the\n"
-                  "data was initialized.")
-            return NotImplemented
-
         # Recursion for multiple sources.
         if isinstance(names, list):
 
@@ -284,75 +289,20 @@ class Survey:
 
             # Loop over names.
             for i, name in enumerate(names):
-                self.add_sources(name, coords[:, i])
+                self._add_sources(name, coords[:, i])
 
         else:
             # Warn about duplicate names.
-            if names in self.sources:
-                old_coords = self.sources[names].coordinates
+            if names in self._sources:
+                old_coords = self._sources[names].coordinates
                 new_coords = tuple(np.array(coordinates, dtype=float))
                 print(f"* WARNING :: Overwriting existing source <{names}>:\n"
                       f"             - Old: {old_coords}\n"
                       f"             - New: {new_coords}")
 
             # Create a receiver dipole.
-            self.sources[names] = Dipole(names, coordinates)
+            self._sources[names] = Dipole(names, coordinates)
 
-    def add_frequencies(self, frequencies):
-        """TODO
-
-        Parameters
-        ----------
-        """
-
-        if self._data is not None:
-            print("Frequencies can, currently, only be added before the\n"
-                  "data was initialized.")
-            return NotImplemented
-
-        # Recursion for multiple sources.
-        if isinstance(frequencies, list):
-
-            # Loop over names.
-            for frequency in frequencies:
-                self.add_frequencies(frequency)
-
-        else:
-            # Create a receiver dipole.
-            self.frequencies[float(frequencies)] = {}
-
-    def add_data(self, source, receiver, frequency, data=None):
-        """Add receiver(s) to the survey.
-
-        Parameters
-        ----------
-        source, receiver : str
-            Source and receiver name(s).
-
-        frequencies : float or array
-            Frequencies [Hz].
-
-        data : None or array (nsrc, nfreq, nrec)
-            Measured data.
-
-        """
-
-        # Initialize xarray.
-        if self._data is None:
-            self._data = xr.Dataset(
-                    {'data': xr.DataArray(
-                        np.ones(self.shape, dtype=complex)*np.nan,
-                        dims=('sources', 'receivers', 'frequencies'))},
-                    coords={
-                        'sources': list(self.sources.keys()),
-                        'receivers': list(self.receivers.keys()),
-                        'frequencies': list(self.frequencies.keys())
-                        },
-                    )
-
-        # Set data.
-        self._data.data.loc[
-                source, receiver, float(frequency)] = np.asarray(data, complex)
 
     @property
     def shape(self):
@@ -361,17 +311,12 @@ class Survey:
         Note that not all source-receiver-frequency pairs do actually have
         data. Check `size` to see how many data points there are.
         """
-        return (len(self.sources), len(self.receivers), len(self.frequencies))
+        return self.data.shape
 
     @property
     def size(self):
         """Return actual data size (does NOT equal nsrc x nrec x nfreq)."""
-        # TODO: Again, data should move to a 1D array, and then we can simply
-        #       return self.data.size.
-        if self._data is None:
-            return 0
-        else:
-            return np.sum(np.isfinite(self.data.data.values))
+        return int(self.data.count())
 
     @property
     def data(self):
@@ -381,7 +326,19 @@ class Survey:
         #   Change to index with unsigned int16 (0-65,535)
         # Currently probably both slow and heavy on RAM.
 
-        return self._data
+        return self._ds.data
+
+    @property
+    def sources(self):
+        return self._sources
+
+    @property
+    def receivers(self):
+        return self._receivers
+
+    @property
+    def frequencies(self):
+        return self._frequencies
 
 
 # # Sources and Receivers # #
