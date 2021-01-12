cd401febbac77a4e0a608c504aaac174d6214444
prisae <mail@werthmuller.org>
2021-01-12 07:16:46 +0100

    Fix test

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -159,7 +159,7 @@ def test_arbitrarily_shaped_source():
     scomp2 = fields.get_source_field(grid, src, freq, strength, msrc=True)
 
     assert_allclose(sman.field, scomp.field)
-    assert_allclose(scomp2.vector, scomp.vector, rtol=1e-6)
+    assert_allclose(scomp2.vector, scomp.vector, rtol=1e-6, atol=1e-15)
 
     # Normalized
     sman = fields.SourceField(grid, freq=freq)
