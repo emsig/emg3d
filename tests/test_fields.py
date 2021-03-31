533501042d118475b74a833e1c84cb7a824f94bd
prisae <mail@werthmuller.org>
2021-03-31 23:15:27 +0200

    Try fixing windows build IX

--- tests/test_fields.py
+++ tests/test_fields.py
@@ -1,4 +1,3 @@
-import sys
 import pytest
 import numpy as np
 from numpy.testing import assert_allclose
@@ -7,34 +6,25 @@ import emg3d
 from emg3d import fields
 
 
-def test_runs_warnings():
-    # The interpolation happens in maps.interp_spline_3d.
-    # Here we just check the 'other' things: warning and errors, and that
-    # it is composed correctly.
+def test_basics_I():
 
-    # Check cubic spline runs fine (NOT CHECKING ACTUAL VALUES!.
-    grid = emg3d.TensorMesh(
-            [np.ones(4), np.array([1, 2, 3, 1]), np.array([2, 1, 1, 1])],
-            [0, 0, 0])
-    field = fields.Field(grid)
-    field.field = np.ones(field.field.size) + 1j*np.ones(field.field.size)
+    x = np.array([400, 450, 500, 550])
+    rec = (x, x*0, 0, 20, 70)
+    res = 0.3
+    src = (0, 0, 0, 0, 0)
+    freq = 10
 
-    grid = emg3d.TensorMesh(
-            [np.ones(6), np.array([1, 1, 2, 3, 1]),
-                np.array([1, 2, 1, 1, 1])], [-1, -1, -1])
-    efield = fields.Field(grid, frequency=1)
-    n = efield.field.size
-    efield.field = np.ones(n) + 1j*np.ones(n)
+    grid = emg3d.construct_mesh(
+            frequency=freq,
+            center=(0, 0, 0),
+            properties=res,
+            domain=[[0, 1000], [-25, 25], [-25, 25]],
+            min_width_limits=20,
+    )
 
-    # Provide wrong rec_loc input:
-    with pytest.raises(ValueError, match='`receiver` needs to be in the'):
-        fields.get_receiver(efield, (1, 1, 1))
 
-@pytest.mark.skipif(sys.platform == 'win32',
-                    reason="does not run on windows")
-def test_basics():
+def test_basics_I():
 
-    # Coarse check with emg3d.solve and empymod.
     x = np.array([400, 450, 500, 550])
     rec = (x, x*0, 0, 20, 70)
     res = 0.3
@@ -54,27 +44,147 @@ def test_basics():
     efield = emg3d.solve(model, sfield, semicoarsening=True,
                             sslsolver=True, linerelaxation=True, verb=1)
 
-    # epm = empymod.bipole(src, rec, [], res, freq, verb=1)
+
+def test_basics_I():
+
+    x = np.array([400, 450, 500, 550])
+    rec = (x, x*0, 0, 20, 70)
+    res = 0.3
+    src = (0, 0, 0, 0, 0)
+    freq = 10
+
+    grid = emg3d.construct_mesh(
+            frequency=freq,
+            center=(0, 0, 0),
+            properties=res,
+            domain=[[0, 1000], [-25, 25], [-25, 25]],
+            min_width_limits=20,
+    )
+
+    model = emg3d.Model(grid, res)
+    sfield = fields.get_source_field(grid, src, freq)
+    efield = emg3d.solve(model, sfield, semicoarsening=True,
+                            sslsolver=True, linerelaxation=True, verb=1)
+
     epm = np.array([-1.27832028e-11+1.21383502e-11j,
                     -1.90064149e-12+7.51937145e-12j,
                     1.09602131e-12+3.33066197e-12j,
                     1.25359248e-12+1.02630145e-12j])
     e3d = fields.get_receiver(efield, rec)
 
-    # 10 % is still OK, grid is very coarse for fast comp (2s)
     assert_allclose(epm, e3d, rtol=0.1)
 
-    # Test with a list of receiver instance.
+
+def test_basics_I():
+
+    x = np.array([400, 450, 500, 550])
+    rec = (x, x*0, 0, 20, 70)
+    res = 0.3
+    src = (0, 0, 0, 0, 0)
+    freq = 10
+
+    grid = emg3d.construct_mesh(
+            frequency=freq,
+            center=(0, 0, 0),
+            properties=res,
+            domain=[[0, 1000], [-25, 25], [-25, 25]],
+            min_width_limits=20,
+    )
+
+    model = emg3d.Model(grid, res)
+    sfield = fields.get_source_field(grid, src, freq)
+    efield = emg3d.solve(model, sfield, semicoarsening=True,
+                            sslsolver=True, linerelaxation=True, verb=1)
+
+    epm = np.array([-1.27832028e-11+1.21383502e-11j,
+                    -1.90064149e-12+7.51937145e-12j,
+                    1.09602131e-12+3.33066197e-12j,
+                    1.25359248e-12+1.02630145e-12j])
+    e3d = fields.get_receiver(efield, rec)
+
+    assert_allclose(epm, e3d, rtol=0.1)
+
     rec_inst = [emg3d.RxElectricPoint((rec[0][i], rec[1][i], *rec[2:]))
                 for i in range(rec[0].size)]
     e3d_inst = fields.get_receiver(efield, rec_inst)
     assert_allclose(e3d_inst, e3d)
 
-    # Only one receiver
+
+def test_basics_I():
+
+    x = np.array([400, 450, 500, 550])
+    rec = (x, x*0, 0, 20, 70)
+    res = 0.3
+    src = (0, 0, 0, 0, 0)
+    freq = 10
+
+    grid = emg3d.construct_mesh(
+            frequency=freq,
+            center=(0, 0, 0),
+            properties=res,
+            domain=[[0, 1000], [-25, 25], [-25, 25]],
+            min_width_limits=20,
+    )
+
+    model = emg3d.Model(grid, res)
+    sfield = fields.get_source_field(grid, src, freq)
+    efield = emg3d.solve(model, sfield, semicoarsening=True,
+                            sslsolver=True, linerelaxation=True, verb=1)
+
+    epm = np.array([-1.27832028e-11+1.21383502e-11j,
+                    -1.90064149e-12+7.51937145e-12j,
+                    1.09602131e-12+3.33066197e-12j,
+                    1.25359248e-12+1.02630145e-12j])
+    e3d = fields.get_receiver(efield, rec)
+
+    assert_allclose(epm, e3d, rtol=0.1)
+
+    rec_inst = [emg3d.RxElectricPoint((rec[0][i], rec[1][i], *rec[2:]))
+                for i in range(rec[0].size)]
+    e3d_inst = fields.get_receiver(efield, rec_inst)
+    assert_allclose(e3d_inst, e3d)
+
     e3d_inst = fields.get_receiver(efield, rec_inst[0])
     assert_allclose(e3d_inst, e3d[0])
 
-    # Ensure responses outside and in the last cell are set to NaN.
+
+def test_basics_I():
+
+    x = np.array([400, 450, 500, 550])
+    rec = (x, x*0, 0, 20, 70)
+    res = 0.3
+    src = (0, 0, 0, 0, 0)
+    freq = 10
+
+    grid = emg3d.construct_mesh(
+            frequency=freq,
+            center=(0, 0, 0),
+            properties=res,
+            domain=[[0, 1000], [-25, 25], [-25, 25]],
+            min_width_limits=20,
+    )
+
+    model = emg3d.Model(grid, res)
+    sfield = fields.get_source_field(grid, src, freq)
+    efield = emg3d.solve(model, sfield, semicoarsening=True,
+                            sslsolver=True, linerelaxation=True, verb=1)
+
+    epm = np.array([-1.27832028e-11+1.21383502e-11j,
+                    -1.90064149e-12+7.51937145e-12j,
+                    1.09602131e-12+3.33066197e-12j,
+                    1.25359248e-12+1.02630145e-12j])
+    e3d = fields.get_receiver(efield, rec)
+
+    assert_allclose(epm, e3d, rtol=0.1)
+
+    rec_inst = [emg3d.RxElectricPoint((rec[0][i], rec[1][i], *rec[2:]))
+                for i in range(rec[0].size)]
+    e3d_inst = fields.get_receiver(efield, rec_inst)
+    assert_allclose(e3d_inst, e3d)
+
+    e3d_inst = fields.get_receiver(efield, rec_inst[0])
+    assert_allclose(e3d_inst, e3d[0])
+
     h = np.ones(4)*100
     grid = emg3d.TensorMesh([h, h, h], (-200, -200, -200))
     model = emg3d.Model(grid)
