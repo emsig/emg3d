9a0433eae17b24827972dbcf9e108ab83fe3db47
Seogi Kang <sgkang09@gmail.com>
2021-08-26 08:38:20 -0700

    back to dieter's implementation, and futher simply (remove conj for real-valued matricies)

--- emg3d/electrodes.py
+++ emg3d/electrodes.py
@@ -731,10 +731,10 @@ class RxMagneticPoint(Receiver, Point):
 
         # h = C*e / (i*omega*mu)
         # smu0 = i*omega*mu
-        h_deriv = (C.T @ P.T).toarray().ravel() * np.conj(-strength)
+        h_deriv = -(C.T @ P.T).toarray().ravel() * strength
 
         # Return adjoint source.
-        return fields.Field(grid=grid, data=np.conj(h_deriv), frequency=frequency)
+        return fields.Field(grid=grid, data=h_deriv, frequency=frequency)
 
 
 # ROTATIONS AND CONVERSIONS
