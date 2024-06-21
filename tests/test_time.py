4c839f720ca37097dc941929e3e756b73b5538a7
Dieter Werthmüller <mail@werthmuller.org>
2024-06-21 15:25:10 +0200

    check

--- tests/test_time.py
+++ tests/test_time.py
@@ -28,7 +28,7 @@ class TestFourier:
         assert Fourier.signal == 0        # Impulse respons
         assert_allclose(times, Fourier.time, 0, 0)
         assert Fourier.verb == 3          # Verbose by default
-        assert 'ey 201' in out
+        assert 'ey' in out
         assert 'Req. freq' in out
         assert 'Calc. freq' in out
         assert Fourier.freq_compute.min() >= fmin
