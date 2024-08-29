231baefb4a705a5f26980c1e4b3ba346382c58e6
Dieter Werthmüller <mail@werthmuller.org>
2024-08-29 17:29:27 +0200

    Don't set _finite if no data

--- emg3d/surveys.py
+++ emg3d/surveys.py
@@ -716,9 +716,15 @@ class Survey:
     @property
     def isfinite(self):
         """Return indices of the finite data."""
-        if not hasattr(self, '_isfinite'):
-            self._isfinite = np.isfinite(self.data.observed.data)
-        return self._isfinite
+        if hasattr(self, '_isfinite'):
+            return self._isfinite
+        else:
+            finite = np.isfinite(self.data.observed.data)
+            # Only set the marker if there is any data.
+            # Otherwise, this was called before observations were added.
+            if finite.sum() > 0:
+                self._isfinite = finite
+            return finite
 
     def finite_data(self, data='observed'):
         """Return finite elements of selected `data`."""
